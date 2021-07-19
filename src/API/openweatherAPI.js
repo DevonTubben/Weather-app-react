0b021325f75241d47907a36770e694c181703beb
Devon Tubben <tubbend1@msu.edu>
2021-07-19 10:20:38 -0400

    added forecast api

--- src/API/openweatherAPI.js
+++ src/API/openweatherAPI.js
@@ -7,5 +7,10 @@ return axios.get(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${API_KEY}`
 )
 }
+function getForecast(lat, lon){ 
+    return axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+}
+
+
 
 export { getCurrentWeather }
