4adfcfeebec7b854b79e1c9761ea067b3dc44cef
Devon Tubben <tubbend1@msu.edu>
2021-07-19 11:13:17 -0400

    hourly forecast now shows. Icons dont work

--- src/Components/Weather-forecast.js
+++ src/Components/Weather-forecast.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 class Forecast extends React.Component { 
     render(){ 
-        return( 
-            <div className="forecast">
-                <div> forecast 10pm </div>
-                <div> forecast 11pm </div>
-                <div> forecast 12am </div>
+        const forecastItems = this.props.hourlyForecast.map((f) => {
+        // this.props.hourlyForecast.map((f) => {
+            const url= `http://openweathermap.org/img/wn/${f.weather[0]}@2x.png`
+            let ampm = "AM"
+            let hour = new Date(f.dt * 1000).getHours();
+            
+
+            if (hour > 12) { 
+            hour = hour - 12
+            ampm = 'PM'
+            }
+            return(
+            <div className="forecast-item"> 
+                <p className="forecast-item_hour">{hour}:00 {ampm}</p>
+                <p className="forecast-item_temperature"> {f.temperature} </p>
+                <img src={url} alt={f.weather[0].main} />
+                <p className="forecast-item_description">{f.weather[0].main} </p>
             </div>
-        )
+            )
+        })
+
+        return(
+        <div className="forecast">
+           {forecastItems} 
+        </div>)
+        
     }
 }
 
