77c14d23df6a7e16e2e7ced405f9c2084e84b474
Devon Tubben <tubbend1@msu.edu>
2021-07-19 10:06:13 -0400

    weather icon now shows for the city you look up

--- src/Components/Current-Weather.js
+++ src/Components/Current-Weather.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 class CurrentWeather extends React.Component { 
     render(){ 
+        const url= `http://openweathermap.org/img/wn/${this.props.icon}@4x.png`
         return( 
             <div className='current-weather'> 
             <div className='current-weather_content'> 
             <p className="current-weather_temp">{this.props.currentTemperature} </p>
             <p className="current-weather_description"> {this.props.description} </p>
-            <img className="current-weather_icon" url="" />
+            <img className="current-weather_icon" src={url} alt={this.props.description}
+            />
             </div>
             <div>
             <p className="current-weather_feels-like"> Feels Like {this.props.feelsLike} </p>
