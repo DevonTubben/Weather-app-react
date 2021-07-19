173427e61a102a617f9537a887e60911335867ff
Devon Tubben <tubbend1@msu.edu>
2021-07-19 11:20:00 -0400

    fixed errors in console.

--- src/Components/Weather-forecast.js
+++ src/Components/Weather-forecast.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 class Forecast extends React.Component { 
     render(){ 
-        const forecastItems = this.props.hourlyForecast.map((f) => {
-        // this.props.hourlyForecast.map((f) => {
+        const forecastItems = this.props.hourlyForecast.map((f, i) => {
+            const key = `forecast-item_${i}`
             const url= `http://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`
             let ampm = "AM"
             let hour = new Date(f.dt * 1000).getHours();
             
-
             if (hour > 12) { 
             hour = hour - 12
             ampm = 'PM'
             }
             return(
-            <div className="forecast-item"> 
+            <div className="forecast-item" key="{key}"> 
                 <p className="forecast-item_hour">{hour}:00 {ampm}</p>
                 <p className="forecast-item_temperature"> {f.temperature} </p>
                 <img src={url} alt={f.weather[0].main} />
